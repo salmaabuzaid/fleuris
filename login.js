dd915e57e8ef5c152f99f4f2b4eba39a039636c6
salmaabuzaid <salma8abuzaid@gmail.com>
2025-06-05 23:03:39 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,21 @@
-document.getElementById("login-form")?.addEventListener("submit", e => {
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://ervcbdxtlzomgbbfwncl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVydmNiZHh0bH-pvbWdiYmZ3bmNsIiwicm9sZSI6ImFub24iLCJp-YXQiOjE3NDkxMjI2NDMsImV4cCI6Mj-A2NDY5ODY0M30.rr2hpt4QtBK2US92bzYQqh-mIE65VIUpZsFpTbIVBt4w';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const form = document.getElementById('login-form');
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  alert("Logged in as " + document.getElementById("login-username").value);
-});
-document.getElementById("signup-form")?.addEventListener("submit", e => {
-  e.preventDefault();
-  alert("Signed up as " + document.getElementById("signup-username").value);
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const { error, data } = await supabase.auth.signInWithPassword({ email, password });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert('Login successful!');
+    window.location.href = 'index.html';
+  }
 });

edca101e1e968442e0cdcfb9a9d08329a04ad8fb
salmaabuzaid <salma8abuzaid@gmail.com>
2025-06-05 23:10:22 +0300

    Update rewards.js

--- rewards.js
+++ rewards.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://ervcbdxtlzomgbbfwncl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVydmNiZHh0bH-pvbWdiYmZ3bmNsIiwicm9sZSI6ImFub24iLCJp-YXQiOjE3NDkxMjI2NDMsImV4cCI6Mj-A2NDY5ODY0M30.rr2hpt4QtBK2US92bzYQqh-mIE65VIUpZsFpTbIVBt4w';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function addPoints(userId, pointsToAdd) {

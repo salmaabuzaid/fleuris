580bb22c25a01da3ecc618169d39a0cc426cade8
salmaabuzaid <salma8abuzaid@gmail.com>
2025-06-05 23:08:01 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("fleuris-cache").then(cache => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+const CACHE_NAME = 'fleuris-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/login.html',
+  '/signup.html',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });

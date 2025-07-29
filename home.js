617cbba76649eef227f1609b66294cca3806d334
salmaabuzaid <salma8abuzaid@gmail.com>
2025-07-29 17:39:30 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,13 +1,35 @@
-// home.js
-function openSettings() {
-  document.getElementById("settingsModal").classList.remove("hidden");
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-function closeSettings() {
-  document.getElementById("settingsModal").classList.add("hidden");
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyAx7DAvkZupSDXg4aXWvtfSQWhqX32Kwrs",
+  authDomain: "fleuris-3df99.firebaseapp.com",
+  projectId: "fleuris-3df99",
+  storageBucket: "fleuris-3df99.appspot.com",
+  messagingSenderId: "831344111439",
+  appId: "1:831344111439:web:YOUR_APP_ID"
+};
 
-function signOut() {
-  alert("Signing out...");
-  // redirect to login later
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Auth check
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
+
+// Settings modal
+const settingsBtn = document.getElementById('settings-btn');
+const modal = document.getElementById('settings-modal');
+const closeBtn = document.getElementById('close-settings');
+settingsBtn.onclick = () => modal.classList.remove('hidden');
+closeBtn.onclick = () => modal.classList.add('hidden');
+
+// Logout
+document.getElementById("logout-btn").onclick = () => {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
+};
